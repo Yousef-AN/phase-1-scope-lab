caf74567015bbbc0cbcc272452c57a96d9e166e7
Yousef. <kingyousef922@gmail.com>
2023-10-06 11:17:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+  }
+
+function overwriteBestCustomer(name) {
+    window.bestCustomer = name;
+}
+const leastFavoriteCustomer = 'Assignment to constant variable.'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer =  'dd';
+}
+
